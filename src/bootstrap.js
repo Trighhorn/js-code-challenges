254a1a63fd5070b799592ac5936988466a89e348
Abrahan Gonzalez <trighhorn@gmail.com>
2020-02-12 17:42:15 -0700

    added functions for adding with for, while, and do/while loops

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,2 +1,41 @@
-// ES6 code goes here
-console.log('es6 code goes here');
+function foraddtool() {
+  let numbers = [1, 2, 3, 4, 5]
+  let sum = 0
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]
+  }
+  return sum
+}
+
+function whileaddtool() {
+  let numbers = [1, 2, 3, 4, 5]
+  let sum = 0
+  let i = 0;
+  while (i < numbers.length) {
+    sum += numbers[i]
+    i++
+  }
+  return sum
+}
+
+function dowhileaddtool() {
+  let numbers = [1, 2, 3, 4, 5]
+  let sum = 0
+  let i = 0;
+
+  do {
+    sum += numbers[i]
+    i++
+  } while (i < numbers.length)
+  return sum
+}
+
+
+
+
+
+
+
+console.log(foraddtool())
+console.log(whileaddtool())
+console.log(dowhileaddtool())
